66432297d00cf8b85f02307288e5ab37f3bf213d
AllenMurua <allenignacio.m@gmail.com>
2024-08-02 10:14:02 -0400

    First Commmit

---
+++ CUE7/EjercicioCUE7/assets/js/index.js
@@ -0,0 +1,11 @@
+var gate=0;
+
+function menu() {
+    while (gate==0) {
+        var option = prompt("1. Crear usuario\n2. Ver usuarios\n3. Eliminar usuario\n4. Salir");
+        switch (option) {
+            case "1":
+
+        }
+    }
+}
